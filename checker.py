3432464fec43dd43731d68cf67b8f5721a29a441
LianaKhazaliya <khazaliya.liana@gmail.com>
2020-02-23 20:38:11 +0300

    Are you feel this perfect flavor of clean data?

--- checker.py
+++ checker.py
@@ -1,14 +1,39 @@
 from pprint import pprint
 
-with open('brands.csv', mode = 'r') as Infile1:
-    with open('DATA_F.csv', mode = 'r') as Infile2:
-# with open('DATA_FINAL.csv', mode = 'w') as Outfile:
-        cars = {'Brand Mark' : 0}
-        for line in Infile1:
-            cars[line[8:-10]] = 0
-        for line in Infile2:
-            cars[' '.join(line.split(',')[:2])] += 1
+with open('DATA_F.csv', mode = 'r') as Infile:
+    with open('DATA_FINAL.csv', mode = 'w') as Outfile:
+        cnt1, cnt2, cnt3 = 0, 0, 0
+        lines = Infile.read().splitlines()
+        
+        for line in lines:
+            sep_line = line.split(',')
+            
+            if len(sep_line) == 14:
+                if sep_line[3][:5] == 'https':
+                    print(line, file=Outfile)
+                else:
+                    fragment = ' '.join(sep_line[2:4])
+                    new_line = ','.join(sep_line[:2]+[fragment]+sep_line[4:-1]+['NoData', sep_line[-1]])
+                    print(new_line, file=Outfile)
 
-        for i in cars.keys():
-            if cars[i] == 0:
-                print(i)
+            elif len(sep_line) < 14:
+                print(','.join(sep_line[:-1]+['NoData', sep_line[-1]]), file=Outfile)
+   
+            else:
+                i = 0
+                while sep_line[i][:5] != 'https':
+                    i += 1
+                fragment = ' '.join(sep_line[2:i])
+                new_line = ','.join(sep_line[:2]+[fragment]+sep_line[i:])
+                new_sep_line = new_line.split(',')
+                if len(new_sep_line) == 14:
+                    if new_sep_line[3][:5] == 'https':
+                        print(new_line, file=Outfile)
+                    else:
+                        print(new_line)
+                elif len(new_sep_line) < 14:
+                    print(','.join(sep_line[:-1]+['NoData', sep_line[-1]]), file=Outfile)
+                    
+                    
+
+                    
